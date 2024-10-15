0e39692df9db4bd8312fcbf09f03185530844af6
hamzalamin <hamzalamin80@gmail.com>
2024-10-15 16:25:47 +0200

    Add GeneralResultRepository to handle CRUD operations for GeneralResult entity

---
+++ src/main/java/com/wora/repositories/GeneralResultRepository.java
@@ -0,0 +1,7 @@
+package com.wora.repositories;
+
+import com.wora.models.entities.GeneralResult;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GeneralResultRepository extends JpaRepository<GeneralResult, Long> {
+}

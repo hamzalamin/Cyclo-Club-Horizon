727678b5d52b05b847454458af665f1522550a6a
hamzalamin <hamzalamin80@gmail.com>
2024-10-21 15:57:23 +0200

    feat: Implemented GeneralResultMapper with MapStruct

---
+++ src/main/java/com/wora/mappers/GeneralResultMapper.java
@@ -0,0 +1,16 @@
+package com.wora.mappers;
+
+import com.wora.mappers.api.GenericMapper;
+import com.wora.models.dtos.generalResult.CreateGeneralResultDto;
+import com.wora.models.dtos.generalResult.GeneralResultDto;
+import com.wora.models.dtos.generalResult.UpdateGeneralResultDto;
+import com.wora.models.entities.GeneralResult;
+
+public interface GeneralResultMapper extends GenericMapper<GeneralResult, GeneralResultDto> {
+    GeneralResult toEntity(GeneralResultDto dto);
+    GeneralResult createEntity(CreateGeneralResultDto dto);
+    GeneralResult updateEntity(UpdateGeneralResultDto dto);
+    GeneralResultDto toDto(GeneralResult generalResult);
+
+}
+

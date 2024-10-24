c5d62830df22873145e86cd77c50d771bfc4b15d
hamzalamin <hamzalamin80@gmail.com>
2024-10-24 14:19:10 +0200

    Added GET endpoint to select RoundResult By RoundResultId

--- src/main/java/com/wora/controllers/RoundResultController.java
+++ src/main/java/com/wora/controllers/RoundResultController.java
@@ -2,15 +2,14 @@ package com.wora.controllers;
 
 import com.wora.models.dtos.roundResult.CreateRoundResultDto;
 import com.wora.models.dtos.roundResult.RoundResultDto;
+import com.wora.models.entities.embeddables.RoundResultId;
 import com.wora.services.IRoundResultService;
 import com.wora.services.impl.RoundResultService;
+import jakarta.validation.constraints.Positive;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/stage-results")
@@ -22,6 +21,17 @@ public class RoundResultController {
     public ResponseEntity<RoundResultDto> createRoundResult(@RequestBody CreateRoundResultDto createRoundResultDto){
         return new ResponseEntity<>(roundResultService.create(createRoundResultDto), HttpStatus.OK);
     }
+
+    @GetMapping("/{riderId}/{roundId}")
+    public ResponseEntity<RoundResultDto> getRoundResultById(
+            @PathVariable("riderId") @Positive Long riderId,
+            @PathVariable("roundId") @Positive Long roundId)
+    {
+        RoundResultId id = new RoundResultId(riderId, roundId);
+        RoundResultDto roundResult = roundResultService.getById(id);
+        return new ResponseEntity<>(roundResult, HttpStatus.OK);
+    }
+
 
 
 }

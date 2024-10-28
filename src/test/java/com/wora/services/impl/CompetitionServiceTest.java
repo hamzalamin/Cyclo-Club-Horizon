e7c7747981d09f2382a1fcf2df3e1615b0fdef66
hamzalamin <hamzalamin80@gmail.com>
2024-10-28 10:15:00 +0100

    Add test for createCompetition() to create successfully

--- src/test/java/com/wora/services/impl/CompetitionServiceTest.java
+++ src/test/java/com/wora/services/impl/CompetitionServiceTest.java
@@ -9,6 +9,7 @@ import com.wora.models.entities.Competition;
 import com.wora.models.entities.GeneralResult;
 import com.wora.models.entities.Round;
 import com.wora.repositories.CompetitionRepository;
+import jakarta.validation.Validator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,6 +20,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -35,6 +37,9 @@ class CompetitionServiceTest {
 
     @InjectMocks
     private CompetitionService service;
+
+    @Mock
+    private Validator validator;
 
 
     @Test
@@ -81,6 +86,49 @@ class CompetitionServiceTest {
     void createCompetition_ShouldThrowExceptionWhenCompetitionDtoIsNull() {
         CreateCompetitionDto competitionDto = null;
         assertThrows(IllegalArgumentException.class, () -> service.create(competitionDto));
+    }
+
+    @Test
+    @DisplayName("create() Should create successfully")
+    void create_shouldCreateSuccessfully(){
+        String name = "TAWAF al-Maghrib";
+        LocalDate startDate = LocalDate.parse("2020-12-12");
+        LocalDate endDate = LocalDate.parse("2020-12-12");
+        String location = "Maroc";
+        Boolean isClosed = false;
+        CreateCompetitionDto createCompetitionDto = new CreateCompetitionDto(name, startDate, endDate, location, isClosed);
+
+        Competition competitionEntity = new Competition();
+        competitionEntity.setName(name);
+        competitionEntity.setStartDate(startDate);
+        competitionEntity.setEndDate(endDate);
+        competitionEntity.setLocation(location);
+        competitionEntity.setIsClosed(isClosed);
+
+
+        Competition savedCompetition = new Competition();
+        savedCompetition.setName(name);
+        savedCompetition.setStartDate(startDate);
+        savedCompetition.setEndDate(endDate);
+        savedCompetition.setLocation(location);
+        savedCompetition.setIsClosed(isClosed);
+
+        CompetitionDto expectedCompetitionDto = new CompetitionDto(savedCompetition.getId(), name, startDate, endDate, location, isClosed, List.of(), List.of());
+
+        when(competitionMapper.toEntity(createCompetitionDto)).thenReturn(competitionEntity);
+        when(repository.save(competitionEntity)).thenReturn(savedCompetition);
+        when(competitionMapper.toDto(savedCompetition)).thenReturn(expectedCompetitionDto);
+
+        CompetitionDto result = service.create(createCompetitionDto);
+
+        assertNotNull(result);
+        assertEquals(expectedCompetitionDto, result);
+        verify(repository).save(competitionEntity);
+        verify(competitionMapper).toDto(savedCompetition);
+
+
+
+
     }
 
 }
